5ca1d38e71e3da6d449cf039086e547333d0d11e
lihongyang <lihongyang2326771271@aliyun.com>
2020-11-14 12:36:35 +0800

    第n次

--- js/spxq.js
+++ js/spxq.js
@@ -7,9 +7,38 @@ $.get('http://jx.xuzhixiang.top/ap/api/detail.php', {
 }, data => {
     console.log(data);
     let str = $(`
-        
+
     <img src="${data.data.pimg}">
             `);
     $('#midArea').append(str);
 
+})
+
+$(function() {
+    $.ajax({
+        url: "http://jx.xuzhixiang.top/ap/api/productlist.php",
+        type: "get",
+        data: {
+            uid: 43966
+        },
+        success: function(res) {
+            console.log(res.data)
+            let products = res.data;
+
+            let html = "";
+            products.forEach(v => {
+                html +=
+                    `<li>
+                    <a href="zhenspxq2.html?pid=${v.pid}">
+                    <img src="${v.pimg}"alt=""></a>
+                    <div>
+                        <span>${v.pname}</span>
+                        <span>${v.pdesc}</span>
+                        <span>${v.pprice}<i>元起</i></span>
+                    </div>
+                    </li>`
+            })
+            $("#list").html(html);
+        }
+    })
 })
